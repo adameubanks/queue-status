c5990204f7ae4896e98d4933b46b4a060a7f3eeb
KhanradCoder <akhanrade@gmail.com>
2024-07-15 14:20:43 -0400

    add navbar

--- app.rb
+++ app.rb
@@ -8,20 +8,6 @@ if development?
   also_reload './command.rb'
 end
 
-# helpers do
-#   def dashboard_title
-#     "UVA Open OnDemand"
-#   end
-
-#   def dashboard_url
-#     "/pun/sys/dashboard/"
-#   end
-
-#   def title
-#     "Disk Usage"
-#   end
-# end
-
 # Define a route at the root '/' of the app.
 get '/' do
   @command = Command.new
